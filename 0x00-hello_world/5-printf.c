a454f863d6662ea7115955eb795452f3f7cb472c
Jotisfag <fagbolaguntosin65@gmail.com>
2022-12-08 20:21:43 -0800

    Tasks 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,11 @@
  * main - Entry
  * Return:	always (0)
  */
+
 int main(void)
 {
-	puts("Programming is like building a multinligual puzzle,\n");
-	pits("With proper grammar, but the outcome is a piece of art,\n");
+	puts("With proper grammar, but the outcome is a piece of art,\n");
+	puts("With proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
