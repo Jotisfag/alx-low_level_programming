7221bab1398ded9fc58e0972b9f538c0ca3253b8
Jotisfag <fagbolaguntosin65@gmail.com>
2022-12-08 20:08:06 -0800

    Tasks 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                     
- */ 
+#include	<stdio.h>
+
+/**
+ * main - Entry
+ * Return:	always (0)
+ */
 int main(void)
 {
-  printf("with proper grammer, but the outcome is a piece of art,\n");
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	puts("Programming is like building a multinligual puzzle,\n");
+	pits("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
