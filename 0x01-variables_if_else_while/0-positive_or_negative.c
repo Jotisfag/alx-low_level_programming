94d7c9e0feda00af88c2d880be46d0074ed3da22
Jotisfag <fagbolaguntosin65@gmail.com>
2022-12-09 03:11:59 -0800

    Tasks 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,10 @@
 #include <time.>
 
 /**
- * main - prints if the number is positive,negative or 0
+ * main - prints if the number is positive, negative or 0
  * Return: Always 0
  */
+
 int main(voi)
 {
 	int n;
