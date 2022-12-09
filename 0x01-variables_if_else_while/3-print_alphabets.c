cda5f1ad72a8b60cbc037c6c862322147623dad0
Jotisfag <fagbolaguntosin65@gmail.com>
2022-12-09 08:34:13 -0800

    Tasks 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower case and upper case
+ * Return:0 no error, non zero if error
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch;)
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n')
+
+	return (0)
+}
