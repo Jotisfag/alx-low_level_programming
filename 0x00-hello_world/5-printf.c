1f9749f939601da93c1abab043a8380ff1356140
Jotisfag <fagbolaguntosin65@gmail.com>
2022-12-10 22:19:47 -0800

    Tasks 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h> 
 
 /**
- * main - Entry
- * Return: Always (0)
+ * main - entry(print a line of code using putf)
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,n");
-	printf("with proper grammar, but the outcome is a piece of art,n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
