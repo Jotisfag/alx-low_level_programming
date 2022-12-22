4424176e4b51972c98ce29026f42c9d3caccdce1
Jotisfag <fagbolaguntosin65@gmail.com>
2022-12-22 05:08:07 -0800

    Tasks 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,15 @@
 
 void rev_string(char *s)
 {
-int l, i;
-char ch;
+	int l, i;
+	char ch;
 
-/*swap the string by looping to half the string*/
-for (i = 0; i < l / 2; ++i)
-{
-ch = s[i];
-s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
-s[l - 1 - i] = ch;
-}
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < l / 2; ++i)
+	{
+		ch = s[i];
+		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
+		s[l - 1 - i] = ch;
+	}
 
 }
